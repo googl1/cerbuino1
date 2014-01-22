54ea547733147678e2aedeccdd29864bce62ac23
Rick Nitsche <ricknitsche@gmx.de>
2014-01-23 00:14:38 +0100

    working

--- MFConsoleApplication2/Program.cs
+++ MFConsoleApplication2/Program.cs
@@ -2,47 +2,39 @@
 using Microsoft.SPOT;
 using System.Threading;
 
-namespace MFConsoleApplication1
+namespace CerbuinoSPI
 {
-    
-
-
     public class Program
     {
-
       public static void Main()
         {
-          DiscoveryAnalogSignal signal = new DiscoveryAnalogSignal();
-          UInt16[] half_sine = {1,0,0,0,0,0,0,0,0,0,2048,3546,4095,3546,2048};
-          UInt16[] saw = {4095, 4094, 4093, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15,16,17,
-                             18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,
-                             34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,
-                             50,51,52,53,54,55,56,57,58,59,60,512,1024,1536,
-                             2048, 2560, 3072,3584, 4095};
-          String f;
+          // init connection to STM32F4Discovery Board
+          DiscoveryPWM pwm = new DiscoveryPWM();
 
+          // PWM Setting (values from 0 to 100):
+          // duty cycles for 4 channels
+          UInt16[] duty1 = {50,50,50,50};
+          // phase shifts for 4 channels
+          UInt16[] phase1 = {1,2,3,4};
 
-
-          Debug.Print("Creating saw-tooth signal with f in Hz;");
-          signal.sendSignal(saw);
-          for (UInt16 i = 9999; i < 10000; i++)
+          Debug.Print("Sending test pattern");
+        
+          for (UInt16 i = 0; i < 100; i++)
           {
-              Debug.Print("soll= " + i + " Hz");
-              Debug.Print(signal.setFreq(i).ToString() + " Hz");
+              // send pwm settings until successfull transmitted
+              // even if this usually works the first time
+              while (!pwm.sendPWM(100, duty1, phase1)) ;
+
+              Debug.Print("phaseshift");
+              for (int j = 0; j < 4; j++)
+              {
+                  phase1[j] *= 2;
+                  phase1[j] %= 100;
+              }
+                  
+
               Thread.Sleep(2000);
-          }
-
-          signal.sendSignal(half_sine);
-          Debug.Print("Creating half sine with frequency in Hz:");
-          for (UInt16 i = 1000; i < 2000; i++)
-          {
-              f = "soll = " + i.ToString() + " kHz";
-              Debug.Print(f);
-             
-              Debug.Print( signal.setFreq(i).ToString());
-              Thread.Sleep(500);
-          }
-          
+          }      
         }
     }
 }
